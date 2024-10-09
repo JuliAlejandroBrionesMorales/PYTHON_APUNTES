330d1b8a6135dca9f9ce4048c61bf344bb3af0ee
Julio Briones <juliobriones842@gmail.com>
2024-10-09 19:03:15 +0200

    nueva actualización POO

--- 16-POO-clases/main.py
+++ 16-POO-clases/main.py
@@ -1,2 +1,87 @@
 # ----------- PROGRAMACION ORIENTADA A OBJETOS -----------------------
 
+
+# Definir una clase (moldde para crear más objetos de este tipo)
+# (Coche) con características similares
+
+from curses import color_content
+from ipaddress import collapse_addresses
+
+
+class Coche:
+    
+    # Atributos o propiedades (variables)
+    # Características del coche
+    color = 'Rojo'
+    marca = 'Ferrari'
+    modelo = 'Aventador'
+    velocidad = 300
+    caballaje = 500
+    plazas = 2
+    
+    # Método, son acciones que hace le objeto (coche) (funciones)
+    def setColor (self, color):
+        self.color = color
+        
+    def getColor (self):
+        return self.color
+    
+    def setModelo (self, modelo):
+        self.modelo = modelo
+    
+    def getModelo (self):
+        return self.modelo
+    
+    def acelerar (self):
+        self.velocidad += 1
+    
+    def frenar (self):
+        self.velocidad -= 1
+        
+    def getVelocidad (self):
+        return self.velocidad
+
+# fin definición clase
+    
+# Crear objetos / Instaciar la clase
+coche = Coche ()
+print("COCHE 1:")
+
+
+
+# -------------- METODO GET AND SET -----------------
+'''
+Es la opción de meter funciones para poder cambiar atributos de tu coche mediante set (meter cosas nuevas)
+y mediante get (obtener es nuevo valor)
+'''
+
+coche.setColor ("amarillo")
+coche.setModelo("Murcielago")
+
+print(coche.marca, coche.getModelo(), coche.getColor())
+print("Velocidad actual: ", coche.getVelocidad())
+
+coche.acelerar()
+coche.acelerar()
+coche.acelerar()
+coche.acelerar()
+
+coche.frenar()
+
+
+print("Velocidad actual: ", coche.getVelocidad())
+
+print("=========================================")
+
+
+# --------------- CREAR MÁS OBJETOS -----------------------
+coche2 = Coche()
+
+coche2.setColor ('Verde')
+coche2.setModelo ('Gallardo')
+
+print("COCHE 2:")
+print(coche2.getColor())
+print(coche2.marca, coche2.getModelo(), coche2.getColor())
+
+print(type(coche2))
---
+++ 17-POO-constructor/coche.py
@@ -0,0 +1,59 @@
+from turtle import color
+
+
+class Coche:
+    
+    # Atributos o propiedades (variables): características del coche
+    color = 'Rojo'
+    marca = 'Ferrari'
+    modelo = 'Aventador'
+    velocidad = 300
+    caballaje = 500
+    plazas = 2
+    
+    def __init__(self, color, marca, modelo, velocidad, caballaje, plazas):
+        self.color = color
+        self.marca = marca
+        self.modelo = modelo
+        self.velocidad = velocidad
+        self.caballaje = caballaje
+        self.plazas = plazas
+    
+    # Método, son acciones que hace le objeto (coche) (funciones)
+    def setColor (self, color):
+        self.color = color
+        
+    def getColor (self):
+        return self.color
+    
+    def setMarca (self, marca):
+        self.marca = marca
+    
+    def getMarca (self):
+        return self.marca
+    
+    def setModelo (self, modelo):
+        self.modelo = modelo
+    
+    def getModelo (self):
+        return self.modelo
+    
+    def acelerar (self):
+        self.velocidad += 1
+    
+    def frenar (self):
+        self.velocidad -= 1
+        
+    def getVelocidad (self):
+        return self.velocidad
+
+    def getInfo (self):
+        
+        info = "--------- Información del coche --------"
+        info += "\n Color: " + self.getColor ()
+        info += "\n Marca: " + self.getMarca ()
+        info += "\n Modelo: " + self.getModelo ()
+        info += "\n Velocidad: " + str(self.getVelocidad ())
+        
+        return info 
+# fin definición clase
